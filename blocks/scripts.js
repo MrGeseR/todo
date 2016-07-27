4008c27c5425f6af22926696fe9da2e460ffa52e
MrGeseR <geser.nikolaev@gmail.com>
2016-07-27 14:42:20 +0300

    Changed es5 on es6 in js (all what could be changed without problems)

--- blocks/scripts.js
+++ blocks/scripts.js
@@ -9,27 +9,27 @@ var ToDo = function(name) {
 ToDo.count = 0;
 
 var ToDoList = function() {
-	var emptyDiv;
-	for (var i = 0; i < document.getElementsByClassName('newToDo').length; i++) {
+	let emptyDiv;
+	for (let i = 0; i < document.getElementsByClassName('newToDo').length; i++) {
 		if (document.getElementsByClassName('newToDo')[i].hasChildNodes()) {
 			continue;
 		} else {
 		 emptyDiv = i;
 		}
 	}
-	
- 	var div = document.getElementsByClassName('newToDo')[emptyDiv];
 
- 	var a = document.createElement('input');
+ 	let div = document.getElementsByClassName('newToDo')[emptyDiv];
+
+ 	let a = document.createElement('input');
  	a.setAttribute('class','taskInput');
  	a.setAttribute('type', 'text');
  	a.setAttribute('placeholder', 'Что вы хотите сделать?');
  	div.appendChild(a);
 
-	var br = document.createElement('br');
+	let br = document.createElement('br');
 	div.appendChild(br);
 
-	var c = document.createElement('ul');
+	let c = document.createElement('ul');
 	c.setAttribute('class','toDoList');
 	c.innerHTML = 'To Do List:';
 	div.appendChild(c);
@@ -41,26 +41,25 @@ var ToDoList = function() {
 	var taskName = document.getElementsByClassName('taskInput')[id];
 	var ul = document.getElementsByClassName('toDoList')[id];
 	var array = [];
-	var arrayDone = [];
 
 	taskName.onkeyup= function () {
 		if(event.keyCode==13) {
-			var task =  document.createElement('li');
-		task.setAttribute('class','toDoList__item')
+			let task =  document.createElement('li');
+			task.setAttribute('class','toDoList__item');
 			if (taskName.value) {
-				var select = document.createElement('input');
+				let select = document.createElement('input');
 				select.setAttribute('type','checkbox');
 				select.setAttribute('class','toDoList__itemSelect');
 				select.setAttribute('id', ToDo.count);
 				task.appendChild(select);
 
-		    var span = document.createElement('label');
+		    let span = document.createElement('label');
 		    span.setAttribute('for',ToDo.count);
 				span.setAttribute('class','toDoList__itemText');
 		    span.innerHTML = taskName.value;
 				task.appendChild(span);
 
-				var deleting = document.createElement('input');
+				let deleting = document.createElement('input');
 				deleting.setAttribute('type','button');
 				deleting.setAttribute('class',' toDoList__itemRemove');
 				deleting.setAttribute('value','Х');
@@ -68,34 +67,34 @@ var ToDoList = function() {
 				task.appendChild(deleting);
 
 				ul.appendChild(task);
-		    var name = taskName.value;
+		    let name = taskName.value;
 		  } else {
 		  	alert('Введите что вы хотите сделать!')
 			}
 			taskName.value = '';
-			var a = new ToDo(name);
+			let a = new ToDo(name);
 			array.push(a);
 		};
 	};
 
 
-	var del = document.createElement('input');
+	let del = document.createElement('input');
  	del.setAttribute('type','button');
  	del.setAttribute('class','button button_delete');
  	del.setAttribute('value','Delete');
  	div.appendChild(del);
 
- 	var done = document.createElement('input');
+ 	let done = document.createElement('input');
  	done.setAttribute('type','button');
  	done.setAttribute('class','button button_done');
  	done.setAttribute('value','Done');
  	div.appendChild(done);
 
- 	var removing = document.getElementsByClassName('button_delete')[id];
- 	var doing = document.getElementsByClassName('button_done')[id];
+ 	let removing = document.getElementsByClassName('button_delete')[id];
+ 	let doing = document.getElementsByClassName('button_done')[id];
 
  	removing.onclick = function (){
- 		for(var i = ul.children.length-1; i >=0; i--) {
+ 		for(let i = ul.children.length-1; i >=0; i--) {
 			if (ul.children[i].children[0].checked) {
  				ul.children[i].remove();
 				continue;
@@ -104,7 +103,7 @@ var ToDoList = function() {
  	}
 
  	doing.onclick = function(){
- 		for(var i = ul.children.length-1; i >=0; i--) {
+ 		for(let i = ul.children.length-1; i >=0; i--) {
  			if (ul.children[i].children[0].checked) {
  				ul.children[i].status = true;
  				ul.children[i].setAttribute('class','toDoList__item toDoList__item_done');
@@ -117,4 +116,4 @@ var ToDoList = function() {
 };
 ToDoList.count = 0;
 
-var newList = new ToDoList();
+var list1 = new ToDoList;
